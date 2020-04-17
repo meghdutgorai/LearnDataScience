6f463740eccb036a5659ad447fcb80ebb1a42cf2
meghdutgorai <meghdutgorai@gmail.com>
2020-04-18 00:18:44 +0530

    Updating from Laptop

--- Data Science Learning Scrips.R
+++ Data Science Learning Scrips.R
@@ -54,3 +54,138 @@ gapminder %>%
   filter(year == past_year & !is.na(gdp)) %>%
   ggplot(aes(log2(dollars_per_day))) +
   geom_histogram(binwidth = 1, color = "black")
+
+## 9.7 Comparing multiple distributions with boxplots and ridge plots ##
+
+gapminder <- gapminder %>%
+  mutate(group = case_when(
+    region %in% c("Western Europe", "Northern Europe","Southern Europe",
+                  "Northern America",
+                  "Australia and New Zealand") ~ "West",
+    region %in% c("Eastern Asia", "South-Eastern Asia") ~ "East Asia",
+    region %in% c("Caribbean", "Central America",
+                  "South America") ~ "Latin America",
+    continent == "Africa" &
+      region != "Northern Africa" ~ "Sub-Saharan",
+    TRUE ~ "Others"))
+
+
+gapminder <- gapminder %>%
+  mutate(group = factor(group, levels = c("Others", "Latin America",
+                                          "East Asia", "Sub-Saharan",
+                                          "West")))
+
+## 9.7.1 Boxplots ##
+
+p <- gapminder %>%
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(group, dollars_per_day)) +
+  geom_boxplot() +
+  scale_y_continuous(trans = "log2") +
+  xlab("") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+p
+
+p + geom_point(alpha = 0.5)
+
+## 9.7.2 Ridge plots ##
+
+p <- gapminder %>%
+  filter(year == past_year & !is.na(dollars_per_day)) %>%
+  ggplot(aes(dollars_per_day, group)) +
+  scale_x_continuous(trans = "log2")
+p + geom_density_ridges()
+
+p + geom_density_ridges(jittered_points = TRUE)
+
+p + geom_density_ridges(jittered_points = TRUE,
+                        position = position_points_jitter(height = 0),
+                        point_shape = '|', point_size = 3,
+                        point_alpha = 1, alpha = 0.7)
+
+## 9.7.3 Example: 1970 versus 2010 income distributions ##
+
+past_year <- 1970
+present_year <- 2010
+years <- c(past_year, present_year)
+gapminder %>%
+  filter(year %in% years & !is.na(gdp)) %>%
+  mutate(west = ifelse(group == "West", "West", "Developing")) %>%
+  ggplot(aes(dollars_per_day)) +
+  geom_histogram(binwidth = 1, color = "black") +
+  scale_x_continuous(trans = "log2") +
+  facet_grid(year ~ west)
+
+country_list_1 <- gapminder %>%
+  filter(year == past_year & !is.na(dollars_per_day)) %>%
+  pull(country)
+country_list_2 <- gapminder %>%
+  filter(year == present_year & !is.na(dollars_per_day)) %>%
+  pull(country)
+country_list <- intersect(country_list_1, country_list_2)
+
+
+gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  ggplot(aes(group, dollars_per_day)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_y_continuous(trans = "log2") +
+  xlab("") +
+  facet_grid(. ~ year)
+
+gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  mutate(year = factor(year)) %>%
+  ggplot(aes(group, dollars_per_day, fill = year)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_y_continuous(trans = "log2") +
+  xlab("")
+
+gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  ggplot(aes(dollars_per_day)) +
+  geom_density(fill = "grey") +
+  scale_x_continuous(trans = "log2") +
+  facet_grid(. ~ year)
+
+
+gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  mutate(group = ifelse(group == "West", "West", "Developing")) %>%
+  ggplot(aes(dollars_per_day, fill = group)) +
+  scale_x_continuous(trans = "log2") +
+  geom_density(alpha = 0.2) +
+  facet_grid(year ~ .)
+
+## 9.7.4 Accessing computed variables ##
+
+p <- gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  mutate(group = ifelse(group == "West", "West", "Developing")) %>%
+  ggplot(aes(dollars_per_day, y = ..count.., fill = group)) +
+  scale_x_continuous(trans = "log2", limit = c(0.125, 300))
+p + geom_density(alpha = 0.2) +
+  facet_grid(year ~ .)
+
+p + geom_density(alpha = 0.2, bw = 0.75) + facet_grid(year ~ .)
+
+gapminder %>%
+  filter(year %in% years & !is.na(dollars_per_day)) %>%
+  ggplot(aes(dollars_per_day, group)) +
+  scale_x_continuous(trans = "log2") +
+  geom_density_ridges(adjust = 1.5) +
+  facet_grid(. ~ year)
+
+gapminder %>%
+  filter(year %in% years & country %in% country_list) %>%
+  group_by(year) %>%
+  mutate(weight = population/sum(population)*2) %>%
+  ungroup() %>%
+  ggplot(aes(dollars_per_day, fill = group)) +
+  scale_x_continuous(trans = "log2", limit = c(0.125, 300)) +
+  geom_density(alpha = 0.2, bw = 0.75, position = "stack") +
+  facet_grid(year ~ .)
+
+
